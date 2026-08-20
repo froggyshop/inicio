3dc304a92bad0813b4388a880937d90691664615
FroggyShop <froggyshop255@gmail.com>
2026-08-19 20:24:49 -0600

    Update products.js

--- js/products.js
+++ js/products.js
@@ -58,7 +58,7 @@ const PRODUCTS = [
     category: "decoracion",
     group: "",
     member: "",
-    images: [assets/productos/planilla-stickers.jpg],
+    images: [assets/productos/planilla-stickers.jpeg],
     price: 15,
     unit: "pieza",
     minOrder: 4,
