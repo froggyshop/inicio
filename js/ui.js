35908e045800d5c7d194060c85df6e8b07f882c5
FroggyShop <froggyshop255@gmail.com>
2026-08-21 20:49:17 -0600

    Eliminación de mensaje "Recuerda leer terminos y condiciones"

--- js/ui.js
+++ js/ui.js
@@ -119,7 +119,6 @@ const UI = {
           <a href="aviso-privacidad.html">Aviso de privacidad</a>
           <a href="terminos.html">Términos y condiciones</a>
         </nav>
-        <p class="footer-note">Recuerda leer nuestros">términos y condiciones</a>.</p>
         <p class="footer-note">© ${new Date().getFullYear()} Froggy Shop.</p>
       </div>
     `;
